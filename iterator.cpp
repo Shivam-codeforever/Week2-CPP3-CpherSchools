146ea35ed21700410259fb550f40848b5c7a0eb0
Shivam Sharma <84848085+Shivam-codeforever@users.noreply.github.com>
2023-02-04 23:55:12 +0530

    Add files via upload

---
+++ iterator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iterator> 
+#include<vector> 
+using namespace std;
+int main()
+{
+    vector<int> ar = { 1, 2, 3, 4, 5 };
+      
+
+    vector<int>::iterator ptr = ar.begin();
+    vector<int>::iterator ftr = ar.end();
+     
+     
+   
+    auto it = next(ptr, 3);
+      
+    
+    auto it1 = prev(ftr, 3);
+      
+   
+    cout << "The position of new iterator using next() is : ";
+    cout << *it << " ";
+    cout << endl;
+      
+    
+    cout << "The position of new iterator using prev()  is : ";
+    cout << *it1 << " ";
+    cout << endl;
+      
+    return 0; 
+}
